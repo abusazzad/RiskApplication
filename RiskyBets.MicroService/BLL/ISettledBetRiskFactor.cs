3ab53db6ac15621324a02e364ecf5aee51048f86
Abu Sazzad <sazzad07@hotmail.com>
2016-09-28 01:17:36 +1000

    Added missing files

---
+++ RiskyBets.MicroService/BLL/ISettledBetRiskFactor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskyBets.MicroService.BLL
+{
+    public interface ISettledBetRiskFactor
+    {
+        int RiskPercentage { get; }
+    }
+}
---
+++ RiskyBets.MicroService/BLL/SettledBetRiskFactors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiskyBets.MicroService.BLL
+{
+    public class SettledBetRiskFactors: ISettledBetRiskFactor
+    {
+        public int RiskPercentage { get { return 60;} }
+    }
+}
